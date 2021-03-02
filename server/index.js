18cd36d83e58dd6d97e1224d66aab0da86a094e5
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-01 22:03:51 -0800

    added in create and find all routes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 // initialize app
 const app = express();
+
+app.use('/posts', postRoutes); // every route inside of postRoutes will start with /posts (localhost:5000/posts)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.dslbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) // to connect to db
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false) // to prevent warnings in console     
 // connect our server application with DB
