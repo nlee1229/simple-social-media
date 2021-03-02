84c2fc741317770ee846bffc49d7c7dfd6c60409
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-01 22:40:37 -0800

    styling additions

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Post from './Post/Post';
+import makeStyles from './styles';
+
+const Posts = () => { 
+    const classes = makeStyles();
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+        </>
+    );
+}
+
+export default Posts;
