2e876cb54936e4eefb1f8b59e172fbd609a7ad8d
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2021-03-01 19:39:06 -0800

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+// here is where we connect our react application to index.html file
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
